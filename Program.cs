29f0aafb15f856bc25025f41544495b0b57ed60a
ZandrexQX <91482671+ZandrexQX@users.noreply.github.com>
2023-02-20 06:50:37 +0300

    Task 56

--- Program.cs
+++ Program.cs
@@ -49,3 +49,39 @@ PrintMatrixInt(matrix);
 Console.ReadLine();
 
 /*--------------------------------------------*/
+
+Console.Clear();
+Console.WriteLine("Task 56");
+Console.WriteLine("------");
+
+void EditMatrix2(int [,] matrix){
+    int minSum = int.MaxValue, numStr = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < matrix.GetLength(1); j++){
+            sum += matrix[i,j];
+        }
+        if (sum < minSum){
+            numStr = i;
+            minSum = sum;
+        }
+    }
+    Console.WriteLine($"Наименьшая сумма {minSum} в строке {numStr}");
+}
+
+Console.Write("Введите размер прямоугольного массива: ");
+int[] size2 = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+while (size2[0]<=size2[1])
+{
+    Console.Write("Строки должны быть больше столбцов: ");
+    size2 = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+}
+int[,] matrix2 = new int[size2[0], size2[1]];
+InputMatrixInt(matrix2);
+PrintMatrixInt(matrix2);
+EditMatrix2(matrix2);
+
+Console.ReadLine();
+
+/*--------------------------------------------*/
