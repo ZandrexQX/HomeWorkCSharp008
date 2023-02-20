fdc0b47dad458d764f0a2b0af7e59b3ae617fa54
ZandrexQX <91482671+ZandrexQX@users.noreply.github.com>
2023-02-20 07:11:32 +0300

    Task 58

--- Program.cs
+++ Program.cs
@@ -54,7 +54,7 @@ Console.Clear();
 Console.WriteLine("Task 56");
 Console.WriteLine("------");
 
-void EditMatrix2(int [,] matrix){
+void MinSum(int [,] matrix){
     int minSum = int.MaxValue, numStr = 0;
     for (int i = 0; i < matrix.GetLength(0); i++)
     {
@@ -80,7 +80,48 @@ while (size2[0]<=size2[1])
 int[,] matrix2 = new int[size2[0], size2[1]];
 InputMatrixInt(matrix2);
 PrintMatrixInt(matrix2);
-EditMatrix2(matrix2);
+MinSum(matrix2);
+
+Console.ReadLine();
+
+/*--------------------------------------------*/
+
+Console.Clear();
+Console.WriteLine("Task 58");
+Console.WriteLine("------");
+
+int [,] MultiplyMatrix(int [,] matrix1, int [,] matrix2){
+    int[,] newMatrix = new int[matrix1.GetLength(0), matrix1.GetLength(1)];
+        for (int i = 0; i < matrix1.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix2.GetLength(1); j++)
+            {
+                for (int k = 0; k < matrix2.GetLength(0); k++)
+                {
+                    newMatrix[i,j] += matrix1[i,k] * matrix2[k,j];
+                }
+            }
+        }
+    return newMatrix;
+}
+
+Console.Write("Введите размер квадратного массива: ");
+int[] size3 = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+while (size3[0] != size3[1])
+{
+    Console.Write("Введите размер квадратного массива: ");
+    size3 = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+}
+
+int[,] matrix31 = new int[size3[0], size3[1]];
+int[,] matrix32 = new int[size3[0], size3[1]];
+InputMatrixInt(matrix31);
+InputMatrixInt(matrix32);
+PrintMatrixInt(matrix31);
+Console.WriteLine("---------------");
+PrintMatrixInt(matrix32);
+Console.WriteLine("Произведение двух матриц:");
+PrintMatrixInt(MultiplyMatrix(matrix31, matrix32));
 
 Console.ReadLine();
 
